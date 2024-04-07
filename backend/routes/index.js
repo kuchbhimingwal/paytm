86ec5bc48ad233512e7a102bb557fbd30e5c6d12
kuchbhimingwal <shubhammingi@gmail.com>
2024-04-07 13:06:16 -0400

    creted a rout for user so all the request go to user.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 const express = require("espress");
-
+const userrouter = require('./user')
 const router = express.Router();
+
+
+router.get('user', userrouter)
+
 
 module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,5 @@
+const express = require("espress");
+
+const userrouter = express.Router();
+
+module.exports = userrouter;
